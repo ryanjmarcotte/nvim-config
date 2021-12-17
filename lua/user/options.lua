edc28d78340bff7d677dac9a38d6c01626a49801
Ryan J. Marcotte <ryan@ryanjmarcotte.com>
2021-12-17 14:14:32 -0500

    Initial commit

---
+++ lua/user/options.lua
@@ -0,0 +1,6 @@
+-- Highlight all matches on previous search pattern.
+vim.opt.hlsearch = true
+
+-- Enable "smart case" matching behavior when searching.
+-- If an upper case letter is provided, match only upper case. Otherwise, match both lowercase and uppercase.
+vim.opt.smartcase = true
