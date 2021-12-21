ba4e72653b4fb86da62ada3828357e09f2158341
ryan.marcotte <ryan.marcotte@tri.global>
2021-12-21 11:35:05 -0500

    Configure a few basic options about line numbers and tabs

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,3 +4,11 @@ vim.opt.hlsearch = true
 -- Enable "smart case" matching behavior when searching.
 -- If an upper case letter is provided, match only upper case. Otherwise, match both lowercase and uppercase.
 vim.opt.smartcase = true
+
+-- Use spaces instead of tabs.
+-- NOTE: Options that are local to the buffer must be set both locally and globally.
+vim.o.expandtab = true
+vim.bo.expandtab = true
+
+vim.opt.smartindent = true
+vim.opt.number = true
