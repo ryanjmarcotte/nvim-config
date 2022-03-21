7f862b4514d331ce6d04959e2815d95e8c0738b8
ryan.marcotte <ryan.marcotte@tri.global>
2022-03-21 11:29:32 -0400

    Update some keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,6 +5,11 @@ local term_opts = { silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Modes
 --   normal_mode = "n",
 --   insert_mode = "i",
@@ -25,6 +30,10 @@ keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate between buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
 
 -- Insert --
 -- Press jk fast to enter
